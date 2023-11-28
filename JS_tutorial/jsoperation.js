301373cc9c914dced676204bbbbf0cabf9394701
moulsan369 <san.m.106bcl@gmail.com>
2023-11-28 13:15:15 +0700

    Update

---
+++ JS_tutorial/jsoperation.js
@@ -0,0 +1,7 @@
+const a = 10, b = 11, c = 12, d = 13;
+
+let sum = a + b + '<br>'; // sum += a,...
+let div = 12/10; // div /= b,...
+
+document.write(sum);
+document.write(div);
---
+++ JS_tutorial/jsvarialble.js
@@ -0,0 +1,9 @@
+let name = 'Moul San';
+let age = 21;
+let day = 5;
+let month = 5;
+let year = 2002;
+
+let your = `${name}<br> ${age}<br>${day}/${month}/${year}`;
+
+document.write(your);
